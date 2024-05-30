9580f5e04cdf042873578c00586037447034bd27
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-05-31 04:11:42 +0530

    Set the environment

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDF0eE2Zpv527Q3sNcc83hbX-reZM6_h-c",
+    authDomain: "netflix-clone-arw.firebaseapp.com",
+    projectId: "netflix-clone-arw",
+    storageBucket: "netflix-clone-arw.appspot.com",
+    messagingSenderId: "368725694302",
+    appId: "1:368725694302:web:15dd85ad1ec6cfa34aa1cf"
+};
